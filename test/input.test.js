921e6ce50938c635c477e0e879e0ee3272db26d2
hais-hbh <578092215@qq.com>
2021-12-02 17:11:17 +0800

    添加 input 测试用例

---
+++ test/input.test.js
@@ -0,0 +1,78 @@
+import sinon from "sinon";
+
+const expect = chai.expect;
+import Vue from 'vue'
+import Input from '../src/input'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Input', () => {
+    it('存在', () => {
+        expect(Input).to.exist
+    })
+    describe('props', () => {
+        const Constructor = Vue.extend(Input)
+        let vm
+        afterEach(() => {
+            vm.$destroy()
+        })
+        it('可以接收value', () => {
+            vm = new Constructor({
+                propsData: {
+                    value: 'hello world'
+                }
+            }).$mount()
+            const inputElement = vm.$el.querySelector('input')
+            expect(inputElement.value).to.eq('hello world')
+        })
+        it('可以接收disabled', () => {
+            vm = new Constructor({
+                propsData: {
+                    disabled: true
+                }
+            }).$mount()
+            const inputElement = vm.$el.querySelector('input')
+            expect(inputElement.disabled).to.eq(true)
+        })
+        it('可以接收readonly', () => {
+            vm = new Constructor({
+                propsData: {
+                    readonly: true
+                }
+            }).$mount()
+            const inputElement = vm.$el.querySelector('input')
+            expect(inputElement.readOnly).to.eq(true)
+        })
+        it('可以接收error', () => {
+            vm = new Constructor({
+                propsData: {
+                    error: 'error message'
+                }
+            }).$mount()
+            const useElement = vm.$el.querySelector('use')
+            expect(useElement.getAttribute('xlink:href')).to.equal('#icon-error')
+            const errorMessage = vm.$el.querySelector('.errorMessage')
+            expect(errorMessage.innerText).to.equal('error message')
+        })
+    })
+    describe('event', () => {
+        const Constructor = Vue.extend(Input)
+        let vm
+        afterEach(() => {
+            vm.$destroy()
+        })
+        it('支持 change/input/focus/blur 事件', () => {
+            ['change', 'input', 'focus', 'blur'].forEach((eventName) => {
+                vm = new Constructor({}).$mount()
+                const callback = sinon.fake()
+                vm.$on(eventName, callback)
+                let event = new Event(eventName)
+                let inputElement = vm.$el.querySelector('input')
+                inputElement.dispatchEvent(event)
+                expect(callback).to.have.been.calledWith(event)
+            })
+        })
+    })
+
+})
