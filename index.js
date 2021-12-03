13dbe850bf89cab1290ada27958e36b644df6018
hais-hbh <578092215@qq.com>
2021-12-03 11:03:19 +0800

    新增导出

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import Button from './src/button'
 import ButtonGroup from './src/button-group'
 import Icon from './src/icon'
+import Input from './src/input'
+import Col from './src/col'
+import Row from './src/row'
 
-export { Button, ButtonGroup, Icon }
+export { Button, ButtonGroup, Icon, Input, Col, Row }
