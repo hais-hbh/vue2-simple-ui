ea81c7680caac9dfd8bb8e3bc8e42314b5ab0fe1
hais-hbh <578092215@qq.com>
2021-12-03 16:44:58 +0800

    fix

--- src/col.vue
+++ src/col.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="col" :class="colClass"
        :style="colStyle">
-    <div style="border: 1px solid green;height: 100px">
       <slot></slot>
-    </div>
   </div>
 </template>
 <script>
