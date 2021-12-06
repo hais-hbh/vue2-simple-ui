1e465c43bc4cc5632b6a383b65f9b91a1f073954
hais-hbh <578092215@qq.com>
2021-12-06 11:19:44 +0800

    实现autoClose closeButton ebableHtml 和多行文字等功能

--- src/app.js
+++ src/app.js
@@ -1,42 +1,47 @@
-import Vue from 'vue'
-import Button from './button.vue'
-import Icon from './icon.vue'
-import ButtonGroup from './button-group.vue'
-import Input from './input.vue'
-import Row from './row.vue'
-import Col from './col.vue'
-import Layout from './layout.vue'
-import Header from './header.vue'
-import Sider from './sider.vue'
-import Content from './content.vue'
-import Footer from './footer.vue'
+import Vue from "vue";
+import Button from "./button.vue";
+import Icon from "./icon.vue";
+import ButtonGroup from "./button-group.vue";
+import Input from "./input.vue";
+import Row from "./row.vue";
+import Col from "./col.vue";
+import Layout from "./layout.vue";
+import Header from "./header.vue";
+import Sider from "./sider.vue";
+import Content from "./content.vue";
+import Footer from "./footer.vue";
 
-Vue.component('g-button', Button)
-Vue.component('g-icon', Icon)
-Vue.component('g-button-group', ButtonGroup)
-Vue.component('g-input', Input)
-Vue.component('g-row', Row)
-Vue.component('g-col', Col)
-Vue.component('g-header',Header)
-Vue.component('g-footer',Footer)
-Vue.component('g-content',Content)
-Vue.component('g-sider',Sider)
-Vue.component('g-layout',Layout)
+Vue.component("g-button", Button);
+Vue.component("g-icon", Icon);
+Vue.component("g-button-group", ButtonGroup);
+Vue.component("g-input", Input);
+Vue.component("g-row", Row);
+Vue.component("g-col", Col);
+Vue.component("g-header", Header);
+Vue.component("g-footer", Footer);
+Vue.component("g-content", Content);
+Vue.component("g-sider", Sider);
+Vue.component("g-layout", Layout);
 
-import plugin from './plugin'
-Vue.use(plugin)
+import plugin from "./plugin";
+Vue.use(plugin);
 
 new Vue({
-    el: '#app',
-    data: {
-        loading1: false,
-        loading2: false,
-        loading3: false,
-        message: 'hi'
+  el: "#app",
+  data: {
+    loading1: false,
+    loading2: false,
+    loading3: false,
+    message: "hi",
+  },
+  methods: {
+    showToast() {
+      this.$toast("我是<strong>加粗文字</strong>", {
+       enableHtml: true
+      });
     },
-    methods: {
-        showToast() {
-            this.$toast("我是 message")
-        }
-    }
-})
+  },
+  mounted() {
+    this.showToast();
+  },
+});
--- src/toast.vue
+++ src/toast.vue
@@ -1,33 +1,103 @@
 <template>
-  <div class="toast">
-    <slot></slot>
+  <div class="toast" ref="toast">
+    <div class="message">
+      <div v-html="$slots.default[0]" v-if="enableHtml"></div>
+      <slot v-else></slot>
+    </div>
+    <div class="line" ref="line"></div>
+    <span class="close" v-if="closeButton" @click="onClickClose">{{
+      closeButton.text
+    }}</span>
   </div>
 </template>
 
 <script>
 export default {
   name: "g-toast",
+  props: {
+    autoClose: {
+      type: Boolean,
+      default: true,
+    },
+    autoCloseDelay: {
+      type: Number,
+      default: 50,
+    },
+    closeButton: {
+      type: Object,
+      default: () => ({
+        text: "关闭",
+        callback: undefined,
+      }),
+    },
+    enableHtml: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  mounted() {
+    this.updateStyle();
+    this.execAutoClose();
+  },
+  methods: {
+    execAutoClose() {
+      if (this.autoClose) {
+        setTimeout(() => {
+          this.close();
+        }, this.autoCloseDelay * 1000);
+      }
+    },
+    updateStyle() {
+      this.$nextTick(() => {
+        this.$refs.line.style.height =
+          this.$refs.toast.getBoundingClientRect().height + "px";
+      });
+    },
+    close() {
+      this.$el.remove();
+      this.$destroy();
+    },
+    onClickClose() {
+      this.close();
+      this.closeButton &&
+        typeof this.closeButton.callback === "function" &&
+        this.closeButton.callback();
+    },
+  },
 };
 </script>
 
 <style lang="scss" scope>
 $font-size: 14px;
-$toast-height: 40px;
-$toast-bg: rgba(0,0,0,.75);
-.toast{
-    position: fixed;
-    top: 0;
-    left: 50%;
-    transform: translateX(-50%);
-    font-size: $font-size;
-    line-height: 1.8;
-    height: $toast-height;
-    display: flex;
-    align-items: center;
-    background: $toast-bg;
-    box-shadow: 0 0 3px 0 rgba(0,0,0,.5);
-    border-radius: 4px;
-    color: #fff;
-    padding: 0 10px;
+$toast-min-height: 40px;
+$toast-bg: rgba(0, 0, 0, 0.75);
+.toast {
+  position: fixed;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  font-size: $font-size;
+  line-height: 1.8;
+  min-height: $toast-min-height;
+  display: flex;
+  align-items: center;
+  background: $toast-bg;
+  box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);
+  border-radius: 4px;
+  color: #fff;
+  padding: 0 10px;
+  .message {
+    padding: 8px 0;
+  }
+  .close {
+    padding-left: 16px;
+    flex-shrink: 0;
+  }
+  .line {
+    height: 100%;
+    border: 1px solid #666;
+    margin-left: 16px;
+    background: #666;
+  }
 }
 </style>
