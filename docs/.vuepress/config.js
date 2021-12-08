89ccf385b915f46be119580664e5ae0e0a57db12
hais-hbh <578092215@qq.com>
2021-12-08 17:20:57 +0800

    fix build

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   title: "Hello VuePress",
   description: "Just playing around",
+  base: "/vue2-simple-ui/",
   themeConfig: {
     sidebar: [
       { title: "入门", children: ["/install/", "/get-start/"] },
